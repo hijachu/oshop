36eb0481d1421c860595a578c3817852e7b9560a
YuChih Hu <hijac.hu@gmail.com>
2019-09-14 19:41:19 +0800

    project initial

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyB4oxHC3tAOkXwaCvwY5QkDnXHbaZV4nN4",
+    authDomain: "oshop-b386c.firebaseapp.com",
+    databaseURL: "https://oshop-b386c.firebaseio.com",
+    projectId: "oshop-b386c",
+    storageBucket: "",
+    messagingSenderId: "314404508642",
+    appId: "1:314404508642:web:dfb269427c7d586e7ddfbb"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyB4oxHC3tAOkXwaCvwY5QkDnXHbaZV4nN4",
+    authDomain: "oshop-b386c.firebaseapp.com",
+    databaseURL: "https://oshop-b386c.firebaseio.com",
+    projectId: "oshop-b386c",
+    storageBucket: "",
+    messagingSenderId: "314404508642",
+    appId: "1:314404508642:web:dfb269427c7d586e7ddfbb"
+  }
 };
 
 /*
