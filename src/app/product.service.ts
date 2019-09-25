2c389d81506ec8b4314c532546f9fea47e817725
YuChih Hu <hijac.hu@gmail.com>
2019-09-26 07:35:11 +0800

    fix getAll() by using snapshotChange()

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,6 +1,5 @@
 import { AngularFireDatabase } from '@angular/fire/database';
 import { Injectable } from '@angular/core';
-import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -14,6 +13,7 @@ export class ProductService {
   }
 
   getAll() {
-    return this.db.list('/products').valueChanges();
+    // return this.db.list('/products').valueChanges();
+    return this.db.list('/products').snapshotChanges();
   }
 }
