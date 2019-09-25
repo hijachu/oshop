9228de5b1ade876ec2556c5d49ffe84d451f203c
YuChih Hu <hijac.hu@gmail.com>
2019-09-25 21:37:03 +0800

    Display the list of products

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,5 +1,6 @@
 import { AngularFireDatabase } from '@angular/fire/database';
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +10,10 @@ export class ProductService {
   constructor(private db: AngularFireDatabase) { }
 
   create(product) {
-    return this.db.list('/prodcuts').push(product);
+    return this.db.list('/products').push(product);
+  }
+
+  getAll() {
+    return this.db.list('/products').valueChanges();
   }
 }
