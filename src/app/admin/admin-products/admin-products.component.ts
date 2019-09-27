e70ca508357b3e5d2ccb6633e05c79391a488c4c
YuChih Hu <hijac.hu@gmail.com>
2019-09-28 00:28:06 +0800

    Extracting Product interface

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ProductService } from 'src/app/product.service';
 import { Subscription } from 'rxjs';
 import { AngularFireAction, DatabaseSnapshot } from '@angular/fire/database';
+import { Product } from 'src/app/models/product';
 
 @Component({
   selector: 'app-admin-products',
@@ -12,14 +13,14 @@ export class AdminProductsComponent implements OnInit, OnDestroy {
   // products$;
   // products: {title: string}[];
   // products: any[];
-  products: AngularFireAction<DatabaseSnapshot<any>>[];
+  products: AngularFireAction<DatabaseSnapshot<Product>>[];
   filterProducts: any[];
   subscription: Subscription;
 
   constructor(private productService: ProductService) {
     // this.products$ = productService.getAll();
     this.subscription = productService.getAll()
-      .subscribe(products => this.filterProducts = this.products = products);
+      .subscribe((products: AngularFireAction<DatabaseSnapshot<Product>>[]) => this.filterProducts = this.products = products);
   }
 
   filter(query: string) {
