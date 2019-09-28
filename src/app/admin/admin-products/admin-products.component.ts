d513d6fbb27a5866cd2d4c174d2c6ca3b38fca31
YuChih Hu <hijac.hu@gmail.com>
2019-09-28 10:28:33 +0800

    Configuring the data table Feeding the data table Add rxjs-compat

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -3,6 +3,7 @@ import { ProductService } from 'src/app/product.service';
 import { Subscription } from 'rxjs';
 import { AngularFireAction, DatabaseSnapshot } from '@angular/fire/database';
 import { Product } from 'src/app/models/product';
+import { DataTableResource } from 'angular5-data-table';
 
 @Component({
   selector: 'app-admin-products',
@@ -16,11 +17,32 @@ export class AdminProductsComponent implements OnInit, OnDestroy {
   products: AngularFireAction<DatabaseSnapshot<Product>>[];
   filterProducts: any[];
   subscription: Subscription;
+  tableResource: DataTableResource< AngularFireAction<DatabaseSnapshot<Product>> >;
+  items: AngularFireAction<DatabaseSnapshot<Product>>[] = [];
+  itemCount: number;
 
   constructor(private productService: ProductService) {
     // this.products$ = productService.getAll();
     this.subscription = productService.getAll()
-      .subscribe((products: AngularFireAction<DatabaseSnapshot<Product>>[]) => this.filterProducts = this.products = products);
+      .subscribe((products: AngularFireAction<DatabaseSnapshot<Product>>[]) => {
+        this.filterProducts = this.products = products;
+        this.initializeTable(products);
+      });
+  }
+
+  private initializeTable(products: AngularFireAction<DatabaseSnapshot<Product>>[]) {
+    this.tableResource = new DataTableResource(products);
+    this.tableResource.query({ offset: 0 })
+      .then(items => this.items = items);
+    this.tableResource.count()
+      .then(count => this.itemCount = count);
+  }
+
+  reloadItems(params) {
+    if (!this.tableResource) return;
+
+    this.tableResource.query(params)
+      .then(items => this.items = items);
   }
 
   filter(query: string) {
