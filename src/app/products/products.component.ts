39ecc61ac2432292c8a4e550cc0d2f7bdbad98f7
YuChih Hu <hijac.hu@gmail.com>
2019-09-29 21:45:26 +0800

    Filtering the categories(done)

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ProductService } from 'src/app/product.service';
 import { CategoryService } from './../category.service';
 import { ActivatedRoute } from '@angular/router';
+import { Product } from '../models/product';
 
 @Component({
   selector: 'app-products',
@@ -9,7 +10,10 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent {
-  products$;
+  // products$;
+  products: Product[] = [];
+  filteredProducts: Product[] = [];
+
   categories$;
   category: string;
 
@@ -17,12 +21,19 @@ export class ProductsComponent {
     route: ActivatedRoute,
     productService: ProductService,
     categoryService: CategoryService) {
-    this.products$ = productService.getAll();
+
+    // this.products = productService.getAll();
+    productService.getAll()
+      .subscribe(products => this.products = products);
 
     this.categories$ = categoryService.getAll();
 
     route.queryParamMap.subscribe(params => {
       this.category = params.get('category');
+
+      this.filteredProducts = (this.category) ?
+        this.products.filter(p => p.category == this.category) :
+        this.products;
     });
   }
 }
